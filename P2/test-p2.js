5de1c41d4f98a7964340f1a58e090f3e2743b04a
andresmdlc <a.matesanz.2017@alumnos.urjc.es>
2020-05-19 18:23:37 +0200

    probando onclick

--- P2/test-p2.js
+++ P2/test-p2.js
@@ -2,6 +2,15 @@
 console.log("Aquí comienza tu código JS...")
 console.log("¡Que comiencen los juegos del JS!")
 
+//-- Manejador del evento click sobre el párrafo test
+//-- Cada vez aue se hace click en el se invoca a esta funcion
+function manejador_parrafo()
+{
+  console.log("Click sobre el párrafo...")
+}
+
+console.log("Ejecutando js...")
+
 //-- Leer el párrafo identificado como test
 const test = document.getElementById('test')
 
@@ -9,3 +18,10 @@ const test = document.getElementById('test')
 //-- (es la propiedad innerHTML)
 console.log("Párrafo test leido. Dice:")
 console.log(test.innerHTML)
+
+//-- Configurar el manejador para el evento de
+//-- pulsación de botón: que se ejecute la
+//-- funcion manejador_parrafo()
+//-- Va sin paréntesis porque la estamos
+//-- tratando como un objeto
+test.onclick = manejador_parrafo;

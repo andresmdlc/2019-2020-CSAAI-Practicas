06d7faed5c20e0d2c6c6915cdf770773e018ecea
andresmdlc <a.matesanz.2017@alumnos.urjc.es>
2020-05-19 18:47:36 +0200

    lo mismo pero más compacto

--- P2/test-p2.js
+++ P2/test-p2.js
@@ -1,14 +1,3 @@
-//-- Punto de entrada: una vez cargada la página se llama a esta
-console.log("Aquí comienza tu código JS...")
-console.log("¡Que comiencen los juegos del JS!")
-
-//-- Manejador del evento click sobre el párrafo test
-//-- Cada vez aue se hace click en el se invoca a esta funcion
-function manejador_parrafo()
-{
-  console.log("Click sobre el párrafo...")
-}
-
 console.log("Ejecutando js...")
 
 //-- Leer el párrafo identificado como test
@@ -20,8 +9,7 @@ console.log("Párrafo test leido. Dice:")
 console.log(test.innerHTML)
 
 //-- Configurar el manejador para el evento de
-//-- pulsación de botón: que se ejecute la
-//-- funcion manejador_parrafo()
-//-- Va sin paréntesis porque la estamos
-//-- tratando como un objeto
-test.onclick = manejador_parrafo;
+//-- pulsación de botón
+test.onclick = function () {
+  console.log("Click sobre el párrafo...")
+}

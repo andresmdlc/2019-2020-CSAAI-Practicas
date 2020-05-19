d8afe71359143fce4529c99327399b0c45832583
andresmdlc <a.matesanz.2017@alumnos.urjc.es>
2020-05-19 18:16:36 +0200

    leyendo un párrafo

--- P2/test-p2.js
+++ P2/test-p2.js
@@ -1,3 +1,11 @@
 //-- Punto de entrada: una vez cargada la página se llama a esta
 console.log("Aquí comienza tu código JS...")
 console.log("¡Que comiencen los juegos del JS!")
+
+//-- Leer el párrafo identificado como test
+const test = document.getElementById('test')
+
+//-- Mostrar en la consola el contenido del párrafo
+//-- (es la propiedad innerHTML)
+console.log("Párrafo test leido. Dice:")
+console.log(test.innerHTML)

f007e2d8421029472099505445ef18a290a0ad54
andresmdlc <a.matesanz.2017@alumnos.urjc.es>
2020-05-19 23:21:04 +0200

    modificando texto

--- P2/test-p2.js
+++ P2/test-p2.js
@@ -1,16 +1,22 @@
 console.log("Ejecutando js...")
 
-//-- Leer el párrafo identificado como test
-const test = document.getElementById('test')
+//-- Leer/obtener el elemento párrafo
+const parrafo1 = document.getElementById('parrafo1')
+const parrafo2 = document.getElementById('parrafo2')
 
 //-- Mostrar en la consola el contenido del párrafo
 //-- (es la propiedad innerHTML)
-console.log("Párrafo test leido. Dice:")
-console.log(test.innerHTML)
+console.log("Párrafo 1 leido dice:")
+console.log(parrafo1.innerHTML)
+console.log("Párrafo 2 leido dice:")
+console.log(parrafo2.innerHTML)
 
 //-- Configurar el manejador para el evento de
 //-- pulsación de botón. Cada vez que se hace
 //-- click sobre el párrafo aumenta el contador
-test.onclick = function () {
-  console.log("Click sobre el párrafo...")
+parrafo2.onclick = () => {
+  console.log("Click sobre el párrafo 2...")
+
+  //-- Cambiar su texto
+  parrafo1.innerHTML = "Como has clickado en el párrafo 2, hemos cambiado el párrafo 1 MUAJAJAJA"
 }

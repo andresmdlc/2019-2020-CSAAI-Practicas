b9f398e720682ac2e73a24ddeb22e56fa12a5c8b
andresmdlc <a.matesanz.2017@alumnos.urjc.es>
2020-05-19 17:41:44 +0200

    volcando las plantillas en los tests

--- P2/test-p2.js
+++ P2/test-p2.js
@@ -1,7 +1,3 @@
 //-- Punto de entrada: una vez cargada la página se llama a esta
-//-- funcion: ahí comienza nuestro programa principal
-function main()
-{
-  console.log("La página ya está cargada")
-  console.log("Ahora es seguro ejecutar el código js")
-}
+console.log("Aquí comienza tu código JS...")
+console.log("¡Que comiencen los juegos del JS!")

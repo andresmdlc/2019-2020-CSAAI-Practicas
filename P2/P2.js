5b35d4ec662f17ef73b254af84cb2946317e8161
andresmdlc <a.matesanz.2017@alumnos.urjc.es>
2020-05-20 18:34:40 +0200

    calculadora funcional

--- P2/P2.js
+++ P2/P2.js
@@ -1,21 +1,39 @@
 //-- Mostrando mensaje de bienvendida
 console.log("Ejecutando JS...");
 
-const botones = document.getElementsByClassName("digito")
+display1 = document.getElementById("display1")
+suma = document.getElementById("suma")
+igual = document.getElementById("igual")
+clear = document.getElementById("clear")
 
-//-- Función de retrollamada de los botones
-//-- botones de la clase dígito
-function digito(value)
-{
-  console.log("Valor: " + value);
+//-- Crear un array con todos los elementos
+//-- de la clase digito
+digito = document.getElementsByClassName("digito")
+
+for (i=0; i<digito.length; i++) {
+  digito[i].onclick = (ev) => {
+    display1.innerHTML += ev.target.value;
+  }
 }
 
-for (i=0; i<botones.length; i++) {
+//-- Crear un array con todos los elementos
+//-- de la clase operacion
+operacion = document.getElementsByClassName("operacion")
 
-  //-- Establecer la funcion de llamada del boton i
-  //-- El parámetro ev.target contiene el boton
-  //-- que ha recibido el click
-  botones[i].onclick = (ev) => {
-    digito(ev.target.value)
+for (i=0; i<operacion.length; i++) {
+  operacion[i].onclick = (ev) => {
+    display1.innerHTML += ev.target.value;
   }
+}
+
+
+
+//-- Evaluar la expresion
+igual.onclick = () => {
+  display2.innerHTML = eval(display1.innerHTML);
+}
+
+//-- Poner a cero la expresion
+clear.onclick = () => {
+  display1.innerHTML = "0";
 }

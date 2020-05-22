e47dc67e38e514ab422f220bfe7a77bb020ab679
andresmdlc <a.matesanz.2017@alumnos.urjc.es>
2020-05-22 18:39:45 +0200

    Añadiendo más vídeos

--- P4/P4.js
+++ P4/P4.js
@@ -5,6 +5,16 @@ const video1 = document.getElementById("video1")
 video1.width=200;  //-- Tamaño de la pantalla de video
 video1.height=100;
 
+//----- Obtener elemento de video y configurarlo
+const video2 = document.getElementById("video2")
+video2.width=200;  //-- Tamaño de la pantalla de video
+video2.height=100;
+
+//----- Obtener elemento de video y configurarlo
+const video3 = document.getElementById("video3")
+video3.width=200;  //-- Tamaño de la pantalla de video
+video3.height=100;
+
 //-- Imagen estática a mostrar cuando el video no
 //-- ha arrancado
 video1.poster="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png";

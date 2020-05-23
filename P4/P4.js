8552bd0ca29e165328a7d9119128b04bf81f960b
andresmdlc <a.matesanz.2017@alumnos.urjc.es>
2020-05-23 11:04:38 +0200

    Poniendo el código más compacto

--- P4/P4.js
+++ P4/P4.js
@@ -3,5 +3,5 @@ console.log("Ejecutando JS...");
 const canvas = document.getElementById("canvas");
 
 //-- Definir el tamaño del canvas
-canvas.width = 300;
+canvas.width = 100;
 canvas.height = 100;

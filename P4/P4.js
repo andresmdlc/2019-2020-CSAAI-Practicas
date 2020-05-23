b4d2512321ca9af31da25409523065b4771f4d0f
andresmdlc <a.matesanz.2017@alumnos.urjc.es>
2020-05-23 13:22:03 +0200

    Obteniendo los elementos de vídeo

--- P4/P4.js
+++ P4/P4.js
@@ -1,30 +1,22 @@
 console.log("Ejecutando JS...");
 console.log("Dibujando rectángulo...");
 
-const canvas = document.getElementById("canvas");
+//----- Obtener elemento de video y configurarlo
+const video_emision = document.getElementById("video_emision")
+video_emision.width=200;  //-- Tamaño de la pantalla de video
+video_emision.height=100;
 
-//-- Definir el tamaño del canvas
-canvas.width = 300;
-canvas.height = 100;
+//----- Obtener elemento de video y configurarlo
+const video1 = document.getElementById("video1")
+video1.width=200;  //-- Tamaño de la pantalla de video
+video1.height=100;
 
-//-- Obtener el contexto del canvas
-const ctx = canvas.getContext("2d");
+//----- Obtener elemento de video y configurarlo
+const video2 = document.getElementById("video2")
+video2.width=200;  //-- Tamaño de la pantalla de video
+video2.height=100;
 
-//-- Definir un rectangulo de dimensiones 100x50,
-//-- cuya esquina superior izquierda está en (5,5)
-ctx.rect(0,0, 300, 100);
-
-//-- Cambiar el tamaño de la linea del trazo
-ctx.lineWidth = 4;
-
-//-- Dibujar el trazo
-ctx.stroke()
-
-
-
-console.log("Mostrando mensaje...");
-
-//-- Texto solido
-ctx.font = "25px Arial";
-ctx.fillStyle = 'red'
-ctx.fillText("Ola ajjaj", 10, 30);
+//----- Obtener elemento de video y configurarlo
+const video3 = document.getElementById("video3")
+video3.width=200;  //-- Tamaño de la pantalla de video
+video3.height=100;

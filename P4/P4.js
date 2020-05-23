e674d9d8b3826e7219350df26e8d926acebe2584
andresmdlc <a.matesanz.2017@alumnos.urjc.es>
2020-05-23 11:23:05 +0200

    Dibujando rectángulos y mostrando mensajes

--- P4/P4.js
+++ P4/P4.js
@@ -1,7 +1,30 @@
 console.log("Ejecutando JS...");
+console.log("Dibujando rectángulo...");
 
 const canvas = document.getElementById("canvas");
 
 //-- Definir el tamaño del canvas
-canvas.width = 100;
+canvas.width = 300;
 canvas.height = 100;
+
+//-- Obtener el contexto del canvas
+const ctx = canvas.getContext("2d");
+
+//-- Definir un rectangulo de dimensiones 100x50,
+//-- cuya esquina superior izquierda está en (5,5)
+ctx.rect(0,0, 300, 100);
+
+//-- Cambiar el tamaño de la linea del trazo
+ctx.lineWidth = 4;
+
+//-- Dibujar el trazo
+ctx.stroke()
+
+
+
+console.log("Mostrando mensaje...");
+
+//-- Texto solido
+ctx.font = "25px Arial";
+ctx.fillStyle = 'red'
+ctx.fillText("Vídeo no encontrado", 10, 30);
--- P4/test-a-p4.js
+++ P4/test-a-p4.js
@@ -1,7 +1,36 @@
 console.log("Ejecutando JS...");
+console.log("Dibujando rectángulo...");
 
 const canvas = document.getElementById("canvas");
 
-//-- Definir el tamaño del canvas
-canvas.width = 300;
+//-- Definir el tamaño del convas
+canvas.width = 200;
 canvas.height = 100;
+
+//-- Obtener el contexto del canvas
+const ctx = canvas.getContext("2d");
+
+//-- Definir un rectangulo de dimensiones 100x50,
+//-- cuya esquina superior izquierda está en (5,5)
+ctx.rect(5,5, 100, 50);
+
+//-- Dibujar
+ctx.fillStyle = 'red';
+
+//-- Cambiar el tamaño de la linea del trazo
+ctx.lineWidth = 4;
+
+//-- Rellenar
+ctx.fill();
+
+//-- Dibujar el trazo
+ctx.stroke()
+
+
+
+console.log("Dibujando texto...");
+
+//-- Texto solido
+ctx.font = "25px Arial";
+ctx.fillStyle = 'yellow'
+ctx.fillText("Texto sólido", 10, 30);

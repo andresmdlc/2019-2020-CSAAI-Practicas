9a2fe983e48170f5eb3e0fef6aa595bf4550d0c0
andresmdlc <a.matesanz.2017@alumnos.urjc.es>
2020-05-25 00:03:33 +0200

    Ahora se puede usar el deslizador rojo

--- P5/filtro.js
+++ P5/filtro.js
@@ -5,6 +5,16 @@ const canvas = document.getElementById('canvas');
 const img_original = document.getElementById('img_original1')
 const ctx = canvas.getContext('2d');
 
+//-- Acceso a los deslizadores
+const deslizador_rojo = document.getElementById('deslizador_rojo');
+const deslizador_verde = document.getElementById('deslizador_verde');
+const deslizador_azul = document.getElementById('deslizador_azul');
+
+//-- Valor de los deslizadores
+const range_value_rojo = document.getElementById('range_value_rojo');
+const range_value_verde = document.getElementById('range_value_verde');
+const range_value_azul = document.getElementById('range_value_azul');
+
 //-- Función de retrollamada de imagen cargada
 //-- La imagen no se carga instantaneamente, sino que
 //-- lleva un tiempo. Sólo podemos acceder a ella una vez
@@ -22,5 +32,33 @@ img_original.onload = function () {
   //-- No se han hecho manipulaciones todavia
   ctx.drawImage(img_original, 0,0);
 };
+
+//-- Funcion de retrollamada del deslizador_rojo
+deslizador_rojo.oninput = () => {
+  //-- Mostrar el nuevo valor del deslizador_rojo
+  range_value_rojo.innerHTML = deslizador_rojo.value;
+
+  //-- Situar la imagen original en el canvas
+  //-- No se han hecho manipulaciones todavia
+  ctx.drawImage(img_original, 0,0);
+
+  //-- Obtener la imagen del canvas en pixeles
+  let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+  //-- Obtener el array con todos los píxeles
+  let data = imgData.data
+
+  //-- Obtener el umbral de rojo del desliador
+  umbral = deslizador_rojo.value
+
+  //-- Filtrar la imagen según el nuevo umbral
+  for (let i = 0; i < data.length; i+=4) {
+    if (data[i] > umbral)
+      data[i] = umbral;
+  }
+
+  //-- Poner la imagen modificada en el canvas
+  ctx.putImageData(imgData, 0, 0);
+}
 
 console.log("Fin...");

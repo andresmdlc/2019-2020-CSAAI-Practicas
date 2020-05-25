d67dd050992a518cda981eef5b16f1b6b6724429
andresmdlc <a.matesanz.2017@alumnos.urjc.es>
2020-05-25 22:28:11 +0200

    Cambiando la imagen cuando no hay emisión

--- P4/P4.js
+++ P4/P4.js
@@ -5,26 +5,26 @@ console.log("Ejecutando JS...");
 //-- OBTENIENDO LOS VÍDEOS Y BOTONES
 
 //-- Acceso a los botones de selección de modo
-const boton_modo_estandar = document.getElementById("boton_modo_estandar")
-const boton_modo_automatico = document.getElementById("boton_modo_automatico")
-const boton_modo_bucle = document.getElementById("boton_modo_bucle")
 const boton_play1 = document.getElementById("boton_play1")
 const boton_play2 = document.getElementById("boton_play2")
 const boton_play3 = document.getElementById("boton_play3")
 const boton_play4 = document.getElementById("boton_play4")
 const boton_stop = document.getElementById("boton_stop")
+//const boton_modo_estandar = document.getElementById("boton_modo_estandar")
+//const boton_modo_automatico = document.getElementById("boton_modo_automatico")
+//const boton_modo_bucle = document.getElementById("boton_modo_bucle")
 
 //----- Obtener elemento de video y configurarlo
 const video_emision = document.getElementById("video_emision")
-video_emision.width=888;  //-- Tamaño de la pantalla de video
-video_emision.height=444;
+video_emision.width=852;  //-- Tamaño de la pantalla de video
+video_emision.height=480;
 //-- Imagen estática a mostrar cuando no se ha elegido fuente
-video_emision.poster="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png";
+video_emision.poster="img_sin_emision.jpg";
 
 //----- Obtener elemento de video y configurarlo
 const video1 = document.getElementById("video1")
 video1.width=213;  //-- Tamaño de la pantalla de video
-video1.height=160;
+video1.height=120;
 video1.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4"
 //-- Reproducir el vídeo directamente nada más cargar la página
 video1.play();
@@ -32,7 +32,7 @@ video1.play();
 //----- Obtener elemento de video y configurarlo
 const video2 = document.getElementById("video2")
 video2.width=213;  //-- Tamaño de la pantalla de video
-video2.height=160;
+video2.height=120;
 video2.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4"
 //-- Reproducir el vídeo directamente nada más cargar la página
 video2.play();
@@ -40,10 +40,12 @@ video2.play();
 //----- Obtener elemento de video y configurarlo
 const video3 = document.getElementById("video3")
 video3.width=213;  //-- Tamaño de la pantalla de video
-video3.height=160;
+video3.height=120;
 video3.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4"
 //-- Reproducir el vídeo directamente nada más cargar la página
 video3.play();
+
+
 
 //-- Estados la realización de TV
 //const ESTADO = {

f0f140cde307ceac74ca4613774adde22c384617
andresmdlc <a.matesanz.2017@alumnos.urjc.es>
2020-05-25 19:17:18 +0200

    Añadiendo estados y acceso a los botones

--- P4/P4.js
+++ P4/P4.js
@@ -4,10 +4,20 @@ console.log("Ejecutando JS...");
 
 //-- OBTENIENDO LOS VÍDEOS Y BOTONES
 
+//-- Acceso a los botones de selección de modo
+const boton_modo_estandar = document.getElementById("boton_modo_estandar")
+const boton_modo_automatico = document.getElementById("boton_modo_automatico")
+const boton_modo_bucle = document.getElementById("boton_modo_bucle")
+const boton_play1 = document.getElementById("boton_play1")
+const boton_play2 = document.getElementById("boton_play2")
+const boton_play3 = document.getElementById("boton_play3")
+const boton_play4 = document.getElementById("boton_play4")
+const boton_stop = document.getElementById("boton_stop")
+
 //----- Obtener elemento de video y configurarlo
 const video_emision = document.getElementById("video_emision")
-video_emision.width=200;  //-- Tamaño de la pantalla de video
-video_emision.height=100;
+video_emision.width=400;  //-- Tamaño de la pantalla de video
+video_emision.height=200;
 //-- Imagen estática a mostrar cuando no se ha elegido fuente
 video_emision.poster="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png";
 
@@ -35,14 +45,43 @@ video3.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4"
 //-- Reproducir el vídeo directamente nada más cargar la página
 video3.play();
 
+//-- Estados la realización de TV
+const ESTADO = {
+  INIT: 0,
+  ESTANDAR: 1,
+  AUTOMATICO: 2,
+  BUCLE: 3,
+}
 
 
-//-- Obtener los botones
-const boton_play1 = document.getElementById("boton_play1")
-const boton_play2 = document.getElementById("boton_play2")
-const boton_play3 = document.getElementById("boton_play3")
-const boton_play4 = document.getElementById("boton_play4")
-const boton_stop = document.getElementById("boton_stop")
+
+function funcion_estandar() {
+
+}
+
+
+
+//-- Variable de estado
+//-- Arrancamos desde el estado inicial
+let estado = ESTADO.INIT;
+
+//-- Modo estándar
+boton_modo_estandar.onclick = () => {
+  estado = ESTADO.ESTANDAR;
+  funcion_estandar();
+}
+
+//-- Modo automático
+boton_modo_automatico.onclick = () => {
+  estado = ESTADO.AUTOMATICO;
+  //funcion_automatico();
+}
+
+//-- Modo bucle
+boton_modo_bucle.onclick = () => {
+  estado = ESTADO.BUCLE;
+  //funcion_bucle();
+}
 
 
 
